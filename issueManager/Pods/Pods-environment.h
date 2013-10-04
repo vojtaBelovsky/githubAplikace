0bf1a952351982501c75409e171d3be71e9c9688
vojtaBelovsky <vojta.belovsky@seznam.cz>
2013-10-04 14:32:15 +0200

    bylo treba prejit na starsi verzi AFNetworkingu

--- issueManager/Pods/Pods-environment.h
+++ issueManager/Pods/Pods-environment.h
@@ -6,11 +6,29 @@
 // project.
 
 
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
 // MTLabel
 #define COCOAPODS_POD_AVAILABLE_MTLabel
 #define COCOAPODS_VERSION_MAJOR_MTLabel 0
 #define COCOAPODS_VERSION_MINOR_MTLabel 0
 #define COCOAPODS_VERSION_PATCH_MTLabel 1
+
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 3
+#define COCOAPODS_VERSION_PATCH_Mantle 0
+
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 3
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 0
 
 // SVPullToRefresh
 #define COCOAPODS_POD_AVAILABLE_SVPullToRefresh
